d1b8c11a807a8917abcf61aa895845359c61ea5a
haruthakobyan108 <hakobyan@harut.me>
2021-06-29 13:39:25 +0400

    add boBack logic to webVew component

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react';
-import { 
+import React, {
+  Component
+} from 'react';
+import {
   StyleSheet,
   Dimensions,
   BackHandler,
   Platform
- } from 'react-native';
-import { WebView } from 'react-native-webview';
+} from 'react-native';
+import {
+  WebView
+} from 'react-native-webview';
 
 const styles = StyleSheet.create({
   flexContainer: {
@@ -29,22 +33,47 @@ export default class App extends Component {
   componentDidMount() {
     if (Platform.OS === 'android') {
       BackHandler.addEventListener('hardwareBackPress', this.onAndroidBackPress);
+    } else {
+      BackHandler.addEventListener('hardwareBackPressCheckpointOverview', this.backPressed);
     }
   }
 
   componentWillUnmount() {
     if (Platform.OS === 'android') {
       BackHandler.removeEventListener('hardwareBackPress');
+    } else {
+      BackHandler.removeEventListener('hardwareBackPressCheckpointOverview', this.backPressed);
     }
   }
 
   render() {
-    return (
-        <WebView startInLoadingState={true} style = {{marginTop: 30, width: Dimensions.get('window').width, height: Dimensions.get('window').height}} 
-        source={{ uri: 'https://eco.am'}}
-        ref={(webView) => { this.webView.ref = webView; }}
-        onNavigationStateChange={(navState) => { this.webView.canGoBack = navState.canGoBack; }}
-        />
+    return ( <
+      WebView startInLoadingState = {
+        true
+      }
+      style = {
+        {
+          marginTop: 30,
+          width: Dimensions.get('window').width,
+          height: Dimensions.get('window').height
+        }
+      }
+      allowsBackForwardNavigationGestures source = {
+        {
+          uri: 'https://eco.am'
+        }
+      }
+      ref = {
+        (webView) => {
+          this.webView.ref = webView;
+        }
+      }
+      onNavigationStateChange = {
+        (navState) => {
+          this.webView.canGoBack = navState.canGoBack;
+        }
+      }
+      />
     );
   }
 }
